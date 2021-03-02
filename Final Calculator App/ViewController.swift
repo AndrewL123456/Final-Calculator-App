1ae3531023c1f6a17739c5ae5155c845433f15d7
Andrew Lujan <106396@psdschools.org>
2021-03-02 09:12:30 -0700

    Initial Commit

---
+++ Final Calculator App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Calculator App
+//
+//  Created by Lujan, Andrew - Student on 3/2/21.
+//  Copyright © 2021 Lujan, Andrew - Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Final Calculator AppTests/Final_Calculator_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Final_Calculator_AppTests.swift
+//  Final Calculator AppTests
+//
+//  Created by Lujan, Andrew - Student on 3/2/21.
+//  Copyright © 2021 Lujan, Andrew - Student. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Calculator_App
+
+class Final_Calculator_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
